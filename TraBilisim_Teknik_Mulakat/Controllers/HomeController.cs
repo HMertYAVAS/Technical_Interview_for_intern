a7f2cdcccc55e76151829f2f80ef040f5eac7be9
H. Mert YAVAŞ <hmertyavas@hotmail.com>
2021-07-09 03:52:13 +0300

    Add project files.

---
+++ TraBilisim_Teknik_Mulakat/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TraBilisim_Teknik_Mulakat.Models;
+
+namespace TraBilisim_Teknik_Mulakat.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly TRABILISIMEntities1 db;
+        public HomeController(){
+        db = new TRABILISIMEntities1();
+    }
+    public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SarkiKaydet(Muzik muzik)
+        {
+            if (ModelState.IsValid) { 
+            db.Muziks.Add(muzik);
+            db.SaveChanges();
+            return Content("Başarılı");
+            }
+            else
+                return Content("Başarısız");
+
+        }
+
+        [HttpGet]
+        public JsonResult SarkiListele()
+        {
+            return Json(db.Muziks.ToList(), JsonRequestBehavior.AllowGet);
+    }
+        [HttpPost]
+        public ActionResult SarkiSil(int id)
+        {
+                var muzik = db.Muziks.Where(x => x.Id == id).FirstOrDefault();
+                if (muzik != null)
+                {
+                    db.Entry(muzik).State = EntityState.Deleted;
+                    db.SaveChanges();  
+                    return Content("Silindi.");
+                }
+            return Content("Silinemedi.");
+        }
+    }
+    }
---
+++ TraBilisim_Teknik_Mulakat/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TraBilisim_Teknik_Mulakat.MvcApplication" Language="C#" %>
